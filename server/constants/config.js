a2366c591de89445c0abbfb1beab5827196de098
abdullahabdulrehman1 <abdullahabdurrehman72@gmail.com>
2024-09-20 03:17:41 +0500

    config_updated

--- server/constants/config.js
+++ server/constants/config.js
@@ -2,6 +2,8 @@ export const corsOptions = {
   origin: [
     "http://localhost:5173",
     "http://localhost:4173",
+    "https://chatappsocketclient.vercel.app",
+
     process.env.CLIENT_URL,
   ],
   credentials: true,
