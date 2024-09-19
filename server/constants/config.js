db62486d74a46ddd6e7da602a974dc1d762eefe1
abdullahabdulrehman1 <abdullahabdurrehman72@gmail.com>
2024-09-20 02:25:54 +0500

    deployment config

--- server/constants/config.js
+++ server/constants/config.js
@@ -1,5 +1,8 @@
 export const corsOptions = {
-  origin: "*",
+  origin: [
+    "http://localhost:5173",
+    "http://localhost:4173",
+    process.env.CLIENT_URL,
+  ],
   credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE"],
 };
