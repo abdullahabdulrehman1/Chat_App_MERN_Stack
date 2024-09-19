fa091533cd59188d6ea76c1d6c942b402092805d
abdullahabdulrehman1 <abdullahabdurrehman72@gmail.com>
2024-09-20 01:38:12 +0500

    deployment config

--- server/app.js
+++ server/app.js
@@ -31,9 +31,9 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server, { cors: corsOptions });
 app.set("io", io);
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors(corsOptions));
 
 app.use(express.urlencoded({ extended: true }));
 const PORT = process.env.PORT || 3000;
@@ -123,3 +123,4 @@ server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT} in ${process.env.NODE_ENV}`);
   userRoute;
 });
+export default app;
--- server/constants/config.js
+++ server/constants/config.js
@@ -1,10 +1,5 @@
-export const corsOptions   ={
-    
-        origin: [
-          "http://localhost:5173",
-          "http://localhost:4173",
-          process.env.CLIENT_URL,
-        ],
-        credentials: true,
-      
-}
+export const corsOptions = {
+  origin: "*",
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+};
