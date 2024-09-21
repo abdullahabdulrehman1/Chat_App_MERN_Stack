6ec978d891b29a129a92c790c8d2826802ed9ea1
abdullahabdulrehman1 <abdullahabdurrehman72@gmail.com>
2024-09-21 23:21:46 +0500

    update

--- server/app.js
+++ server/app.js
@@ -33,11 +33,7 @@ const app = express();
 const server = createServer(app);
 
 const corsOptions = {
-  origin: [
-    "http://localhost:5173",
-    "http://localhost:4173",
-    process.env.CLIENT_URL,
-  ],
+  origin: [process.env.CLIENT_URL],
   credentials: true,
 };
 
